873e9bb886280de903e1367e2e661830052ebd1b
touhidulfahim <muhdtohidul@gmail.com>
2019-02-07 20:39:41 +0600

    git commit method overloading

---
+++ OOPPracticeApp/Models/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPracticeApp.Models
+{
+    public class Calculator
+    {
+        //method overloading technique
+        public int Add(int a, int b) //same method dif type of two parameters
+        {
+            return (a + b);
+        }
+        public float Add(float a, float b, float c) //same method dif type of three parameters
+        {
+            return (a + b + c);
+        }
+        public double Add(double a, double b, double c, double d) //same method dif type of four parameters
+        {
+            return (a + b + c + d);
+        }
+    }
+}
--- OOPPracticeApp/Program.cs
+++ OOPPracticeApp/Program.cs
@@ -11,11 +11,21 @@ namespace OOPPracticeApp
     {
         static void Main(string[] args)
         {
-            Product product = new Product();
-            product.ProductName = "Nokia 3310";
-            product.Price = 5000;
-            var abc = "Product is :"+product.ProductName+" & its price is :"+product.Price+"Tk";
-            Console.WriteLine(abc);
+            //Product product = new Product();
+            //product.ProductName = "Nokia 3310";
+            //product.Price = 5000;
+            //var abc = "Product is :"+product.ProductName+" & its price is :"+product.Price+"Tk";
+
+            //*****************Method Overloading**********************//
+            //It's Compile-time polymorphism, Static Polymorphism, Early to Bind
+            //Number of parameters, Order Of Parameters, Dif data type of parameters
+
+            Calculator c = new Calculator();
+            int A=c.Add(10 , 15);
+            float B=c.Add(10, 15, 20);
+            double C=c.Add(10.1, 15.2, 20.3, 25.4);
+            var result="the int total is: "+A+"\n the float total is: "+B+"\n the double total is: "+C;
+            Console.WriteLine(result);
             Console.ReadKey();
         }
     }
