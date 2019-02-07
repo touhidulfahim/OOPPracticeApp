051e5d433f79f04c5c74766362bd66dd2079814d
touhidulfahim <muhdtohidul@gmail.com>
2019-02-07 20:05:12 +0600

    git commit class and object

--- OOPPracticeApp/Program.cs
+++ OOPPracticeApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OOPPracticeApp.Models;
 
 namespace OOPPracticeApp
 {
@@ -10,6 +11,12 @@ namespace OOPPracticeApp
     {
         static void Main(string[] args)
         {
+            Product product = new Product();
+            product.ProductName = "Nokia 3310";
+            product.Price = 5000;
+            var abc = "Product is :"+product.ProductName+" & its price is :"+product.Price+"Tk";
+            Console.WriteLine(abc);
+            Console.ReadKey();
         }
     }
 }
